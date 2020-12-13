d3c1c622df6a7c809b380cc69da0123db9795b79
sy-n00 <70579136+sy-n00@users.noreply.github.com>
2020-12-13 16:43:50 +0900

    Update recommend.php

--- recommend.php
+++ recommend.php
@@ -9,7 +9,7 @@
   $result = mysqli_query($link, $query);
   print_r($result);
   $row = mysqli_fetch_array($result);
-  rint_r($row);
+  print_r($row);
   #$valid = $row['count(rental_num)'];
   $valid = $row[0];
   print_r($valid);
